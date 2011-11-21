40e09a045379311d68e9a36643015fd2dbc8f9c6
= <=>
2011-11-20 17:49:57 -0800

    Changing seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+#MUST BE CHANGED WITHIN APP AS SOON AS DEPLOYMENT FINISHES
+admin_user = User.create(:name => "Admin user", :email => "jskirst@gmail.com", :password => "password", :password_confirmation => "password")
