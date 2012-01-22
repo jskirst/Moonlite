aa70a24550536bffea8a99601f8224135d8f7807
Jonathan Kirst <jskirst@gmail.com>
2012-01-21 16:41:17 -0800

    Changing the google analytics back to the projectmoonlit.com token.

--- public/javascripts/core.js
+++ public/javascripts/core.js
@@ -1,11 +1,11 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-26236755-1']);
+_gaq.push(['_setAccount', 'UA-28492737-1']);
 _gaq.push(['_trackPageview']);
 
 (function() {
-	var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 
 $('#topbar').scrollSpy();
