988f37b4eee8d20c496d1be6190ab41126840444
= <=>
2012-05-02 16:28:12 -0700

    more.

--- db/migrate/20120430225344_remove_owner_id_from_tables.rb
+++ db/migrate/20120430225344_remove_owner_id_from_tables.rb
@@ -1,17 +1,17 @@
 class RemoveOwnerIdFromTables < ActiveRecord::Migration
 	def change
-		remove_column :achievements, :owner_id, :integer
-		remove_column :companies, :owner_id, :integer
-		remove_column :company_users, :owner_id, :integer
-		remove_column :completed_tasks, :owner_id, :integer
-		remove_column :enrollments, :owner_id, :integer
-		remove_column :info_resources, :owner_id, :integer
-		remove_column :paths, :owner_id, :integer
-		remove_column :rewards, :owner_id, :integer
-		remove_column :sections, :owner_id, :integer
-		remove_column :tasks, :owner_id, :integer
-		remove_column :users, :owner_id, :integer
-		remove_column :user_achievements, :owner_id, :integer
-		remove_column :user_transactions, :owner_id, :integer
+		remove_column :achievements, :owner_id
+		remove_column :companies, :owner_id
+		remove_column :company_users, :owner_id
+		remove_column :completed_tasks, :owner_id
+		remove_column :enrollments, :owner_id
+		remove_column :info_resources
+		remove_column :paths, :owner_id
+		remove_column :rewards, :owner_id
+		remove_column :sections, :owner_id
+		remove_column :tasks, :owner_id
+		remove_column :users, :owner_id
+		remove_column :user_achievements, :owner_id
+		remove_column :user_transactions, :owner_id
 	end
 end
