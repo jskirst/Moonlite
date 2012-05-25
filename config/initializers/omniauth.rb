683351fb9ed86c78f1cc4c4b965185f54484b329
= <=>
2012-05-25 02:30:46 -0700

    Removing SSL.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env == "production"
-    provider :facebook, '276612745757646', '3f9879134b5ebde644389bdf958c45cd', :scope => 'email,user_about_me', :display => 'popup'
+    provider :facebook, '276612745757646', '3f9879134b5ebde644389bdf958c45cd', :scope => 'email,user_about_me', :client_options => {:ssl => {:verify => false}}
   else
     provider :facebook, '276612745757646', '3f9879134b5ebde644389bdf958c45cd', :callback_url => "http://www.metabright.com/locallink/", :client_options => {:ssl => {:verify => false}}, :scope => 'email,user_about_me'
   end
