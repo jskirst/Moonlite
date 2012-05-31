1c6819d6b01aa9c6fbc88baebda6e636b6857472
Jonathan Kirst <jskirst@gmail.com>
2012-05-31 12:21:06 -0700

    Pulling env variable from ENV first.

--- config/initializers/paperclip_s3_patch.rb
+++ config/initializers/paperclip_s3_patch.rb
@@ -1,12 +1,14 @@
-if Rails.env.production?
+current_env = ENV['RACK_ENV'] || Rails.env
+
+if current_env == "production"
 	Paperclip.interpolates(:s3_moonlite_url) { |attachment, style|
 		"#{attachment.s3_protocol}://moonlite.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}"
 	}
-elsif Rails.env.staging?
+elsif current_env == "staging"
 	Paperclip.interpolates(:s3_moonlite_url) { |attachment, style|
 		"#{attachment.s3_protocol}://moonlite-staging.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}"
 	}
-elsif Rails.env.development?
+elsif current_env == "development"
 	Paperclip.interpolates(:s3_moonlite_url) { |attachment, style|
 		"#{attachment.s3_protocol}://moonlite-dev.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}"
 	}
