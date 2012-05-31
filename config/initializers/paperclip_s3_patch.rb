ccee7a40fa1a49b93a0a61a7277ed5632320b5d3
Jonathan Kirst <jskirst@gmail.com>
2012-05-31 12:46:46 -0700

    Correcting double : problem for info resources.

--- config/initializers/paperclip_s3_patch.rb
+++ config/initializers/paperclip_s3_patch.rb
@@ -2,18 +2,18 @@ current_env = ENV['RACK_ENV'] || Rails.env
 
 if current_env == "production"
 	Paperclip.interpolates(:s3_moonlite_url) { |attachment, style|
-		"#{attachment.s3_protocol}://moonlite.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}"
+		"#{attachment.s3_protocol}://moonlite.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}".gsub("::",":")
 	}
 elsif current_env == "staging"
 	Paperclip.interpolates(:s3_moonlite_url) { |attachment, style|
-		"#{attachment.s3_protocol}://moonlite-staging.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}"
+		"#{attachment.s3_protocol}://moonlite-staging.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}".gsub("::",":")
 	}
 elsif current_env == "development"
 	Paperclip.interpolates(:s3_moonlite_url) { |attachment, style|
-		"#{attachment.s3_protocol}://moonlite-dev.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}"
+		"#{attachment.s3_protocol}://moonlite-dev.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}".gsub("::",":")
 	}
 else
 	Paperclip.interpolates(:s3_moonlite_url) { |attachment, style|
-		"#{attachment.s3_protocol}://moonlite-test.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}"
+		"#{attachment.s3_protocol}://moonlite-test.s3.amazonaws.com/#{attachment.path(style).gsub(%r{^/}, "")}".gsub("::",":")
 	}
 end
