6a38822734c92e664ca762ce3b5ce1dfd35da5f6
= <=>
2012-06-13 15:00:57 -0700

    Fixing duplicate path user roles issue.

--- app/models/path_user_role.rb
+++ app/models/path_user_role.rb
@@ -6,5 +6,12 @@ class PathUserRole < ActiveRecord::Base
   
   validates :user_role_id, :presence => true
   validates :path_id, :presence => true
+  validate :check_for_duplicate
   
+  private
+    def check_for_duplicate
+      if PathUserRole.find_by_path_id_and_user_role_id(self.path_id, self.user_role_id)
+        errors[:base] << "This user role has already been given access."
+      end
+    end
 end
