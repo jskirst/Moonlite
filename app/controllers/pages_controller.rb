6762950694b49f0bb09cd648aef97552f7d3efab
Jonathan Kirst <jc@blueigloo.us>
2012-11-06 08:24:42 -0500

    Adding read more.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -13,7 +13,7 @@ class PagesController < ApplicationController
       @votes = current_user.votes.to_a.collect {|v| v.submitted_answer_id } 
       @newsfeed_items = []
       @enrollments.each do |e|
-        @newsfeed_items << e.path.completed_tasks.joins(:submitted_answer).all(order: "completed_tasks.created_at DESC", limit: 10)
+        @newsfeed_items << e.path.completed_tasks.joins(:submitted_answer).all(order: "completed_tasks.created_at DESC", limit: 30)
       end
       @newsfeed_items = @newsfeed_items.flatten.sort { |a, b| a.created_at <=> b.created_at }
       render "users/home"
