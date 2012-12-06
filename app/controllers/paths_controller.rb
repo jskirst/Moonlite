a9345b988712177b90925ed464a9af34c57163d6
Jonathan Kirst <jc@blueigloo.us>
2012-12-06 11:09:24 -0800

    Autodirecting to proper tab after question completion.

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -163,7 +163,7 @@ class PathsController < ApplicationController
       @current_section = current_user.most_recent_section_for_path(@path)
       @votes = current_user.votes.to_a.collect {|v| v.submitted_answer_id }
       @display_launchpad = params[:completed]
-      @display_type = params[:type]
+      @display_type = params[:type] || 2
     end
     
     @page = params[:page].to_i
