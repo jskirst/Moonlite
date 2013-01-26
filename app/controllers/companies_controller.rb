82b1ea0425a33f3216d7e57618fbf67fe417f0e4
Jonathan Kirst <jc@blueigloo.us>
2013-01-26 13:38:12 -0500

    Adding new filters to admin overview.

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -3,17 +3,26 @@ class CompaniesController < ApplicationController
   before_filter { raise "ACCESS DENIED" unless @enable_administration }
   
   def overview
-    @user_count = User.count
-    @ct_count = CompletedTask.count
-    @submission_count = SubmittedAnswer.count
-    @vote_count = Vote.count
-    @comment_count = Comment.count
+    admin_users = [0]
+    unless params[:admin]
+      admin_users = User.joins(:user_role).where("user_roles.enable_administration = ?", true).to_a.collect &:id
+    end
+
+    @users                = User.where("id not in (?)", admin_users).count
+    @arena_answers        = CompletedTask.joins(:task).where("user_id not in (?)", admin_users).where("tasks.answer_type = ?", Task::MULTIPLE).count
+    @creative_responses   = CompletedTask.joins(:task).where("user_id not in (?)", admin_users).where("tasks.answer_type = ?", Task::MULTIPLE).count
+    @completed_tasks      = CompletedTask.joins(:task).where("user_id not in (?)", admin_users).where("tasks.answer_type = ?", Task::MULTIPLE).count
+    @contributions        = Task.where("creator_id not in (?)", admin_users).count
+    @votes                = Vote.where("user_id not in (?)", admin_users).count
+    @comments             = Comment.where("user_id not in (?)", admin_users).count
     
-    @new_user_count = User.where("DATE(created_at) = DATE(?)", Time.now).count
-    @new_ct_count = CompletedTask.where("DATE(created_at) = DATE(?)", Time.now).count
-    @new_submission_count = SubmittedAnswer.where("DATE(created_at) = DATE(?)", Time.now).count
-    @new_vote_count = Vote.where("DATE(created_at) = DATE(?)", Time.now).count
-    @new_comment_count = Comment.where("DATE(created_at) = DATE(?)", Time.now).count
+    @new_users              = User.where("id not in (?)", admin_users).where("DATE(created_at) = DATE(?)", Time.now).count
+    @new_arena_answers      = CompletedTask.joins(:task).where("user_id not in (?)", admin_users).where("tasks.answer_type = ?", Task::MULTIPLE).where("DATE(completed_tasks.created_at) = DATE(?)", Time.now).count
+    @new_creative_responses = CompletedTask.joins(:task).where("user_id not in (?)", admin_users).where("tasks.answer_type = ?", Task::CREATIVE).where("DATE(completed_tasks.created_at) = DATE(?)", Time.now).count
+    @new_completed_tasks    = CompletedTask.joins(:task).where("user_id not in (?)", admin_users).where("tasks.answer_type = ?", Task::CHECKIN).where("DATE(completed_tasks.created_at) = DATE(?)", Time.now).count
+    @new_contributions      = Task.where("creator_id not in (?)", admin_users).where("DATE(completed_tasks.created_at) = DATE(?)", Time.now).count
+    @new_votes              = Comment.where("user_id not in (?)", admin_users).where("DATE(created_at) = DATE(?)", Time.now).count
+    @new_comments           = Vote.where("user_id not in (?)", admin_users).where("DATE(created_at) = DATE(?)", Time.now).count
   end
   
   def users
