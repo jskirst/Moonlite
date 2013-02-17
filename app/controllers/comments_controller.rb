f598356d7464f802fbaee653d9af616fc904e22a
Jonathan Kirst <jc@blueigloo.us>
2013-02-17 15:06:09 -0500

    Knocking out multiple comments.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -8,9 +8,11 @@ class CommentsController < ApplicationController
       if owner.is_a? SubmittedAnswer
         submission_user = comment.owner.user
         link = submission_details_path(owner.path.permalink, owner)
+        alerted_users = []
         comment.owner.comments.each do |c|
-          content = "#{current_user} commented on your submission."
           if c.user != current_user and c.user != submission_user
+            next if alerted_users.include?(c.user)
+            alerted_users << c.user
             UserEvent.log_event(c.user, "#{current_user} replied to your comment on a submission.", current_user, link, current_user.picture)
           end
         end
