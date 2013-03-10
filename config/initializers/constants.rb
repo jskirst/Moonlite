37ddadc8bbfa178ad025318f873c7d4d38585960
Jonathan Kirst <jc@blueigloo.us>
2013-03-09 19:25:45 -0500

    Fixes for formatting changes.

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -38,6 +38,7 @@ end
 LIGHT_BULB_URL    = "https://s3.amazonaws.com/moonlite-nsdub/static/Lightbulb+icon"
 
 STONEY_SMALL_URL  = "https://s3.amazonaws.com/moonlite-nsdub/static/stoney+100x150.png"
+STONEY_MEDIUM_URL = "https://s3.amazonaws.com/moonlite-nsdub/static/stoney_50x66.png"
 
 ICON_CLOSE_URL    = "https://s3.amazonaws.com/moonlite-nsdub/static/close_icon.png"
 ICON_CAMERA_URL   = "https://s3-us-west-1.amazonaws.com/moonlite/static/image.png"
