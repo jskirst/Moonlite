c8b008078f3fa34cfe85653b4f3026406ead8506
Jonathan Kirst <jc@blueigloo.us>
2013-03-14 02:24:07 -0400

    more fixes.

--- app/helpers/paths_helper.rb
+++ app/helpers/paths_helper.rb
@@ -1,26 +1,28 @@
 module PathsHelper
   
   def check_achievements(points, enrollment)
+    achievements = { level: false, rank: false, contribution: false }
+    
     current_score = enrollment.total_points
     previous_score = current_score - points
     
     current_level = enrollment.level
     previous_level = Enrollment.points_to_level(previous_score)
+    if current_level > previous_level
+      achievements[:level] = current_level
+      if current_level > 1 and previous_level == 1
+        achievemens[:contribution] = true
+      end
+    end 
     
     current_rank = enrollment.rank
     previous_rank = Enrollment.rank(previous_score, enrollment.path_id)
+    achievements[:rank] = 100 if current_rank <= 100 and previous_rank > 100
+    achievements[:rank] = 50  if current_rank <= 50 and previous_rank > 50
+    achievements[:rank] = 25  if current_rank <= 25 and previous_rank > 25
+    achievements[:rank] = 10  if current_rank <= 10 and previous_rank > 10
     
-    return false if current_level == previous_level && current_rank == previous_rank 
-    
-    level_jump = current_level if current_level > previous_level
-    
-    rank_jump = 100 if current_rank <= 100 and previous_rank > 100
-    rank_jump = 50  if current_rank <= 50 and previous_rank > 50
-    rank_jump = 25  if current_rank <= 25 and previous_rank > 25
-    rank_jump = 10  if current_rank <= 10 and previous_rank > 10
-    
-    return false if rank_jump.nil? and level_jump.nil?
-    return level_jump, rank_jump
+    return achievements
   end
   
 end
