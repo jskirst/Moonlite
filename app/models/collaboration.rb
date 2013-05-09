5e67a7ca9e6471cec75ebb5e3a61ca7eba5f4aeb
Jonathan Kirst <jc@blueigloo.us>
2013-05-09 18:06:59 -0400

    Collaboration fixes.

--- app/models/collaboration.rb
+++ app/models/collaboration.rb
@@ -9,24 +9,4 @@ class Collaboration < ActiveRecord::Base
   validates :path_id, presence: true, uniqueness: { scope: [:user_id, :granting_user_id] }
   validates :user_id, presence: true
   validates :granting_user_id, presence: true
-  
-  validate :granting_user_can_grant_collaboration
-  validate :user_can_collaborate
-  
-  private
-    def granting_user_can_grant_collaboration
-      if !granting_user.user_role.enable_content_creation
-        errors[:base] << "You must be able to create to add collaborators."
-      elsif self.granting_user_id != path.user_id
-        errors[:base] << "Only creators can add additional collaborators."
-      end
-    end
-    
-    def user_can_collaborate
-      if !user.user_role.enable_content_creation
-        errors[:base] << "You must be able to create to become a collaborator."
-      elsif user.company_id != path.company_id
-        errors[:base] << "Cross organization access is not allowed."
-      end
-    end
 end
