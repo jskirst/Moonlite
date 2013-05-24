4b3fc8f5a626ed2fcf46b7b58f36ebe93b5e9fec
Jonathan Kirst <jc@blueigloo.us>
2013-05-24 15:04:50 -0400

    Adding live metascore variables.

--- app/controllers/sections_controller.rb
+++ app/controllers/sections_controller.rb
@@ -320,6 +320,13 @@ class SectionsController < ApplicationController
   def finish
     @session_id = params[:session_id]
     if params[:s].blank? and current_user.seen_opportunities
+      @enrollment.calculate_metascore
+      @enrollment.calculate_metapercentile
+      
+      @total_session_points = current_user.completed_tasks.where(session_id: @session_id).sum(:points_awarded)
+      @total_challenge_points = @enrollment.total_points
+      @metascore = @enrollment.metascore
+      @metapercentile = @enrollment.metapercentile
       render "finish"
     else
       step = params[:s].to_i
