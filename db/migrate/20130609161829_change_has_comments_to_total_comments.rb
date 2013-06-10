2bfad89f33c6d55c4c101961f692121f5c6bda73
Jonathan Kirst <jc@blueigloo.us>
2013-06-10 09:04:46 -0400

    Removing db row updates.

--- db/migrate/20130609161829_change_has_comments_to_total_comments.rb
+++ db/migrate/20130609161829_change_has_comments_to_total_comments.rb
@@ -1,10 +1,5 @@
 class ChangeHasCommentsToTotalComments < ActiveRecord::Migration
   def change
     add_column :submitted_answers, :total_comments, :integer, default: 0
-
-    SubmittedAnswer.all.each do |sa|
-      sa.total_comments = sa.comments.count
-      sa.save
-    end 
   end
 end
