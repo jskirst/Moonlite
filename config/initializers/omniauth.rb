d408d7363dd8e69c60a49f4c2e16a6d714959cca
Jonathan Kirst <jc@blueigloo.us>
2013-09-27 11:47:02 -0700

    Heroku assets precompile issue.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,20 +1,22 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  %w[ facebook google github linkedin ].each { |provider| return true if ENV["#{provider.upcase}_KEY"].blank? or ENV["#{provider.upcase}_SECRET"].blank? }
+if ENV['FACEBOOK_KEY']
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    #%w[ facebook google github linkedin ].each { |provider| return true if ENV["#{provider.upcase}_KEY"].blank? or ENV["#{provider.upcase}_SECRET"].blank? }
   
-  if Rails.env == "production"
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'email,user_about_me, user_work_history, user_location, user_education_history', :client_options => {:ssl => {:verify => false}}
-    provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], access_type: 'online', approval_prompt: '', client_options: {ssl: {verify: false}}, scope: "userinfo.email, plus.me, userinfo.profile"
-    provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], :scope => 'r_fullprofile r_emailaddress'
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  elsif Rails.env == "staging"
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'email,user_about_me, user_work_history, user_location, user_education_history', :client_options => {:ssl => {:verify => false}}
-    provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], access_type: 'online', approval_prompt: '', client_options: {ssl: {verify: false}}, scope: "userinfo.email, plus.me, userinfo.profile"
-    provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], :scope => 'r_fullprofile r_emailaddress'
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
-  else
-    provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], client_options: {ssl: {verify: false}}, scope: 'email,user_about_me, user_work_history, user_location, user_education_history'
-    provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], access_type: 'online', approval_prompt: '', client_options: {ssl: {verify: false}}, scope: "userinfo.email, plus.me, userinfo.profile"
-    provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], :scope => 'r_fullprofile r_emailaddress'
-    provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
+    if Rails.env == "production"
+      provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'email,user_about_me, user_work_history, user_location, user_education_history', :client_options => {:ssl => {:verify => false}}
+      provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], access_type: 'online', approval_prompt: '', client_options: {ssl: {verify: false}}, scope: "userinfo.email, plus.me, userinfo.profile"
+      provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], :scope => 'r_fullprofile r_emailaddress'
+      provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    elsif Rails.env == "staging"
+      provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], :scope => 'email,user_about_me, user_work_history, user_location, user_education_history', :client_options => {:ssl => {:verify => false}}
+      provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], access_type: 'online', approval_prompt: '', client_options: {ssl: {verify: false}}, scope: "userinfo.email, plus.me, userinfo.profile"
+      provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], :scope => 'r_fullprofile r_emailaddress'
+      provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+    else
+      provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], client_options: {ssl: {verify: false}}, scope: 'email,user_about_me, user_work_history, user_location, user_education_history'
+      provider :google_oauth2, ENV['GOOGLE_KEY'], ENV['GOOGLE_SECRET'], access_type: 'online', approval_prompt: '', client_options: {ssl: {verify: false}}, scope: "userinfo.email, plus.me, userinfo.profile"
+      provider :linkedin, ENV['LINKEDIN_KEY'], ENV['LINKEDIN_SECRET'], :scope => 'r_fullprofile r_emailaddress'
+      provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: "user"
+    end
   end
 end
