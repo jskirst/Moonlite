9e6311464562cbdde9b01cbb9cfb6888df80e610
Jonathan Kirst <jc@blueigloo.us>
2013-09-30 17:14:22 -0700

    Trying to diagnose production cache issue.

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -421,7 +421,6 @@ class PathsController < ApplicationController
 
   private    
     def load_resource
-      return Path.first
       @path = Path.cached_find(params[:permalink] || params[:id])
       unless @path
         @path = Path.find_by_permalink(params[:permalink]) if params[:permalink]
