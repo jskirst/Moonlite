9476722c5b45bfb34b9f822e1176f1851d6136c1
Jonathan Kirst <jc@blueigloo.us>
2013-09-30 17:27:23 -0700

    Diagnosing production issues.

--- app/controllers/paths_controller.rb
+++ app/controllers/paths_controller.rb
@@ -421,6 +421,7 @@ class PathsController < ApplicationController
 
   private    
     def load_resource
+      raise "Unknown cache issue" unless Rails.cache.stats
       @path = Path.cached_find(params[:permalink] || params[:id])
       unless @path
         @path = Path.find_by_permalink(params[:permalink]) if params[:permalink]
