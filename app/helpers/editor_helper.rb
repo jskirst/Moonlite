218765f23204921f9b48357f2968250dd5009ef5
Jonathan Kirst <jc@blueigloo.us>
2013-10-28 12:04:53 -0700

    Fixing task stats.

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -1,4 +1,15 @@
 module EditorHelper
+  def completion_statistics_label(task)
+    cts = task.completed_tasks
+    total_count = cts.count
+    total = total_count == 0 ? 1 : total_count
+    correct = cts.where(status_id: Answer::CORRECT).count
+    incorrect = cts.where(status_id: Answer::INCORRECT).count
+    incomplete = cts.where(status_id: Answer::INCOMPLETE).count
+    str = "#{total_count}/#{(correct.to_f/total).round(0)}/#{(incorrect.to_f/total).round(0)}/#{(incomplete.to_f/total).round(0)}"
+    content_tag(:span, str, class: "label")
+  end
+
   def difficulty_label(task)
     difficulty_class = case task.difficulty
     when Task::EASY
