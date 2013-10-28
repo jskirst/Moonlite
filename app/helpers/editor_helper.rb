d5696bd9e569e3ccfd4d68c8eb371da9bd33e7df
Jonathan Kirst <jc@blueigloo.us>
2013-10-28 12:14:52 -0700

    More task editor fixes.

--- app/helpers/editor_helper.rb
+++ app/helpers/editor_helper.rb
@@ -6,8 +6,8 @@ module EditorHelper
     correct = cts.where(status_id: Answer::CORRECT).count
     incorrect = cts.where(status_id: Answer::INCORRECT).count
     incomplete = cts.where(status_id: Answer::INCOMPLETE).count
-    str = "#{total_count}/#{(correct.to_f/total).round(0)}/#{(incorrect.to_f/total).round(0)}/#{(incomplete.to_f/total).round(0)}"
-    content_tag(:span, str, class: "label")
+    str = "#{total_count} Total / #{((correct.to_f/total)*100).round(0)}% Correct / #{((incorrect.to_f/total)*100).round(0)}% Incorrect / #{((incomplete.to_f/total)*100).round(0)}% Incomplete"
+    content_tag(:p, content_tag(:span, str, class: "label"))
   end
 
   def difficulty_label(task)
