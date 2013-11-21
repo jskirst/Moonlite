5507bc19e6b2ab1ff00bc1421c513e149d7da6d0
nsdub <nathanielsokollward@gmail.com>
2013-11-20 19:04:00 -0800

    fixing word break issue and reporting task issues

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -155,11 +155,11 @@ class TasksController < ApplicationController
   def report
     issue = current_user.task_issues.new(task_id: @task.id, issue_type: params[:task_issue][:issue_type])
     if issue.save
-      flash[:alert] = "Thank you for reporting this question. Once two or more users have reported the same issue, the question will be removed. Thanks for your help keeping MetaBright at its best!"
+      flash[:success] = "Thank you for reporting this question. Once two or more users have reported the same issue, the question will be removed. Thanks for your help keeping MetaBright at its best!"
     else
-      flash[:alert] = "We're sorry, your issue could not be reported. You may have tried to create another issue report for the same question."  
+      flash[:error] = "We're sorry, your issue could not be reported. You may have tried to create another issue report for the same question."  
     end
-    redirect_to challenge_path(@task.path.permalink)
+    redirect_to continue_path_path(@task.path.permalink)
   end
   
   def add_stored_resource
