596abe2c17edbe1c1102de6137eccff041db6bdc
Jonathan Kirst <jc@blueigloo.us>
2013-12-16 13:09:05 -0800

    Forcing SSL for images.

--- app/models/stored_resource.rb
+++ app/models/stored_resource.rb
@@ -12,6 +12,7 @@ class StoredResource < ActiveRecord::Base
     :bucket => ENV['S3_BUCKET_NAME'],
     :path => ":attachment/:id/:style.:extension",
     :url  => ":s3_moonlite_url",
+    :s3_protocol => :https,
     :s3_credentials => {
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
